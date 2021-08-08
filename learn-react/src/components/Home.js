ed475b255773ede0bf659a6f5fec1fd591e0bfb1
mysubsnews <my.subs.news@live.com>
2021-08-08 13:17:17 +0200

    updated react project

---
+++ learn-react/src/components/Home.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+// API
+import API from "../API";
+
+// config
+import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+
+// Components
+
+// Hook
+
+// Image
+import NoImage from "../images/no_image.jpg";
+
+const Home = () => {
+  const [state, setState] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      console.log(movies);
+
+      setState(prev => ({
+        ...movies,
+        results:
+        page > 1 ? [...page] 
+      }))
+
+    } catch (error) {
+      setError(true);
+    }
+  };
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  return <div>HomePage</div>;
+};
+
+export default Home;
